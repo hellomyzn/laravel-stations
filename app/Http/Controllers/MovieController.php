9d3235fbd6381a9d2fd4871a30153591ab62a876
hellomyzn <em711th@icloud.com>
2022-11-18 18:04:47 +1100

    show movie title

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
-        return view("movies.index");
+        $movies = Movie::all();
+
+        return view("movies.index", compact('movies'));
     }
 }
