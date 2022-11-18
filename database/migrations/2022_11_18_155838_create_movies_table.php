4a8420907894dadc65ce11ed80800cd6a2fb095f
hellomyzn <em711th@icloud.com>
2022-11-18 18:00:58 +1100

    migrate

--- database/migrations/2022_11_18_155838_create_movies_table.php
+++ database/migrations/2022_11_18_155838_create_movies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->text('title')->comment('映画タイトル');
+            $table->text('image_url')->comment('画像URL');
             $table->timestamps();
         });
     }
