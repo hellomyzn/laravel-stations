a99fcec19bd8d932f25317b1468f868c08dbdcf7
hellomyzn <em711th@icloud.com>
2022-11-20 07:53:34 +1100

    s13

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Movie;
 
 class Schedule extends Model
 {
     use HasFactory;
+    protected $with = ['movies'];
+
+    public function movie(){
+        return $this->belongsTo(Movie::class);
+    }
 }
