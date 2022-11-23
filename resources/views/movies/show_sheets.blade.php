6e089557d5dca0012a837c5b41888e559fab6e1b
hellomyzn <em711th@icloud.com>
2022-11-23 19:03:11 +1100

    make sheets disable if there is already reservation

--- resources/views/movies/show_sheets.blade.php
+++ resources/views/movies/show_sheets.blade.php
@@ -19,9 +19,14 @@
         @for ($j = 0; $j < 5; $j++)
 
             <td>
+                
                 <form action={{route('movies.reservations.create', ['id' => $id, 'schedule_id' => $schedule_id])}} method="GET">
                     @csrf
-                    <input type="submit" value="{{ $sheets[($i * 5) + $j]->row ."-". $sheets[($i * 5) + $j]->column  }}" class="btn btn-danger post_del_btn">
+                    @if (is_null($sheets[($i * 5) + $j]->reservation))
+                        <input type="submit" value="{{ $sheets[($i * 5) + $j]->row ."-". $sheets[($i * 5) + $j]->column  }}" class="btn btn-danger post_del_btn">
+                    @else        
+                        <input type="button" disabled value="{{ $sheets[($i * 5) + $j]->row ."-". $sheets[($i * 5) + $j]->column  }}" class="btn btn-danger post_del_btn">
+                    @endif
                     <input type="hidden" name="screening_date" value={{$screening_date}}>
                     <input type="hidden" name="sheetId" value={{$sheets[($i * 5) + $j]->id}}>
                 </form>
